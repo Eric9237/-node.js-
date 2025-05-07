a432dbdab6312823a3eaa4416ac56c80ed82b749
Eric9237 <k28828528@gmail.com>
2025-05-07 22:19:01 +0800

    改變MYSQL的路徑到AWS RDS中

--- sql.js
+++ sql.js
@@ -1,11 +1,18 @@
-const mysql = require('mysql')
+const mysql = require('mysql2')
 const db = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'admin123',
+    host: 'nodejs.c10yywu4cx2k.ap-northeast-3.rds.amazonaws.com',
+    user: 'admin',
+    password: '9237eric',
     database: 'my_db_01'
 })
 
-db.connect()
+// db.connect()
 
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to RDS:', err);
+        return;
+    }
+    console.log('Connected to the RDS database!');
+});
 module.exports = db
